a0601b3406d4a04d0f499cfc855d6358aa512e5d
roitk <rohit.kothur@gmail.com>
2018-01-21 08:20:00 -0700

    Add discord to hangouts relaying

--- hangupsbot/plugins/discord.py
+++ hangupsbot/plugins/discord.py
@@ -7,12 +7,12 @@ import plugins
 client = discord.Client()
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-#bot_global = None
+bot_global = None
 
 def _initialize(bot):
     plugins.register_handler(_received_message, type="message", priority=50)
-    #nonlocal bot_global
-    #bot_global = bot
+    global bot_global
+    bot_global = bot
     _start_discord_account(bot)
 
 @client.event
@@ -29,9 +29,8 @@ async def on_message(message):
     author = str(message.author).rsplit('#', 1)[0]
     new_message = author + ": " + content
     logger.info("message received: {}".format(new_message))
-    #for convid in bot_global.conversations.get():
-    #    bot_global.coro_send_message(convid, new_message)
-    # send message to hangouts here
+    for convid in bot_global.conversations.get():
+        bot_global.coro_send_message(convid, new_message)
 
 # Called when the bot starts up
 # Need to set up the discord connection and account here
@@ -47,7 +46,6 @@ def _start_discord_account(bot):
 def _received_message(bot, event, command):
     logger.info("hangouts message handler")
     # send message to discord here
-    # TODO: send only to text channels
     for chan in client.get_all_channels():
         if chan.type == discord.ChannelType.text:
             logger.info(chan)
